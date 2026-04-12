9b414d40b2bc07c13c2d0a0d15ac2f02b423ba74
Bot777777 <scontacts.records@gmail.com>
2026-04-13 00:23:24 +0200

    Add path module and static file serving

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(__dirname));
 // 👤 USER
 let user = {
   balance: 10000,
