e4e9e60651bcbfe5406da024c23ebaa2e6719fc5
Bot777777 <scontacts.records@gmail.com>
2026-04-26 17:45:48 +0200

    Refine trendMove threshold for skipping iterations

--- server.js
+++ server.js
@@ -362,7 +362,7 @@ let prev = h[h.length-2];
 //if(market === "SIDE" && Math.abs(trendMove) < 0.000) continue;
 
  //❌ Bewegung minimal erhöhen
-if(Math.abs(trendMove) < 0.00005) continue;
+if(Math.abs(trendMove) < 0.000005) continue;
 
 // ❌ LONG nur leichter Rücksetzer
 if(decision === "buy"){
