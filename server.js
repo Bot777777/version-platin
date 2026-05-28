4a703bc839491ccd694b63483e6c0f4798a9be4c
Bot777777 <scontacts.records@gmail.com>
2026-05-28 18:04:22 +0200

    Fix conditions for buy and short momentum reversal

--- server.js
+++ server.js
@@ -172,10 +172,10 @@ let prev2 = h[h.length - 3];
 
 // 🔥 LONG MOMENTUM REVERSAL
 if(
-    ema20 < ema50 &&
-   price < prev &&
-    prev < prev2 &&
-    rsi < 50
+    ema20 > ema50 &&
+   price > prev &&
+    prev > prev2 &&
+    rsi > 50
 ){
     return "buy";
 }
@@ -184,9 +184,9 @@ if(
 // 🔥 SHORT MOMENTUM REVERSAL
 if(
     ema20  < ema50 &&
-    price = < prev &&
-    prev = < prev2 &&
-    rsi = < 48
+    price  < prev &&
+    prev  < prev2 &&
+    rsi  <  50
 ){
     return "short";
 }  
